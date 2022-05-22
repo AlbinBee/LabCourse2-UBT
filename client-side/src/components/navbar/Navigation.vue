420f200175e6c6e044e2f739178ba7da2211defb
albin berisha <albin.berisha033@gmail.com>
2022-05-22 21:40:07 +0200

    UI refactoring and changes

--- client-side/src/components/navbar/Navigation.vue
+++ client-side/src/components/navbar/Navigation.vue
@@ -1,87 +1,75 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info" class="p-2">
-      <b-navbar-brand href="#">"Logo"</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" class="justify-content-between" is-nav>
-        <b-navbar-nav>
-          <b-nav-item class="nav-links">
-            <router-link class="router-link" :to="{ name: 'Home' }">
-              Home
-            </router-link>
-          </b-nav-item>
-          <b-nav-item class="nav-links">
-            <router-link class="router-link" :to="{ name: 'Admin' }">
-              Admin
-            </router-link>
-          </b-nav-item>
-          <b-nav-item v-if="!isLoggedIn" class="nav-links">
-            <router-link class="router-link" :to="{ name: 'Login' }">
-              Login
-            </router-link>
-          </b-nav-item>
-          <b-nav-item v-if="!isLoggedIn" class="nav-links">
-            <router-link class="router-link" :to="{ name: 'Register' }">
-              Register
-            </router-link>
-          </b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto align-items-center">
-          <b-nav-form class="">
-            <div class="d-flex justify-content-center">
-              <b-form-input
-                size="sm"
-                class="mr-sm-2"
-                placeholder="Search"
-              ></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit"
-                >Search</b-button
-              >
-            </div>
-          </b-nav-form>
-
-          <!-- <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown> -->
-
-          <b-nav-item v-if="isLoggedIn" class="nav-links">
-            <b-avatar variant="info" :src="userImg" />
-          </b-nav-item>
-
-          <b-nav-item v-if="isLoggedIn" class="nav-links">
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center mr-5">
+        <v-list color="primary" class="d-flex">
+          <v-list-item v-for="item in items" :key="item" :to="{ name: 'Home' }">
+            "logo"
+            <!-- <img :src="logo" alt="logo" class="logo" /> -->
+          </v-list-item>
+        </v-list>
+      </div>
+      <div class="nav-links">
+        <v-list color="primary" class="d-flex">
+          <v-list-item v-for="item in items" :key="item" :to="{ name: item }">
+            {{ item }}
+          </v-list-item>
+        </v-list>
+      </div>
+      <v-spacer></v-spacer>
+      <div class="nav-links">
+        <v-list color="primary" class="d-flex">
+          <v-list-item v-if="isLoggedIn">
+            <v-avatar size="40">
+              <img :src="userImg" />
+            </v-avatar>
+          </v-list-item>
+          <v-list-item v-if="isLoggedIn" class="nav-links">
             {{ firstName }}
-          </b-nav-item>
+          </v-list-item>
+        </v-list>
+      </div>
+      <div class="my-2">
+        <v-btn
+          outlined
+          text
+          v-if="!isLoggedIn"
+          :to="{ name: 'Login' }"
+          class="mr-2"
+        >
+          <span>Login</span>
+          <v-icon right>login</v-icon>
+        </v-btn>
+      </div>
 
-          <b-nav-item
-            v-if="isLoggedIn"
-            class="nav-links"
-            @click="handleSignOut"
-          >
-            Sign Out
-          </b-nav-item>
-          <!-- <b-nav-item-dropdown right>
-            <template #button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown> -->
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+      <div class="my-2">
+        <v-btn outlined text v-if="!isLoggedIn" :to="{ name: 'Register' }">
+          <span>Register</span>
+          <v-icon right>person_add</v-icon>
+        </v-btn>
+      </div>
+
+      <div class="my-2">
+        <v-btn outlined text v-if="isLoggedIn" @click="handleSignOut">
+          <span>Sign Out</span>
+          <v-icon right>logout</v-icon>
+        </v-btn>
+      </div>
+    </v-app-bar>
   </div>
 </template>
 
 <script>
 import { signOut } from "firebase/auth";
+
 export default {
+  components: {},
+  data() {
+    return {
+      items: ["Admin"],
+      logo: require("@/assets/main-logo.svg"),
+    };
+  },
   methods: {
     handleSignOut() {
       signOut(this.auth).then(() => {
@@ -113,11 +101,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav-links {
-  ::v-deep {
-    a {
-      color: white !important;
-    }
-  }
+.logo {
+  max-width: 250px;
 }
 </style>
