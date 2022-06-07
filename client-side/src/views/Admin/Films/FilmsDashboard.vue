4bd4ebcc4fe0549ef79dfe124fb2ea41b0a342cf
albin berisha <albin.berisha033@gmail.com>
2022-06-08 00:22:06 +0200

    Added Layouts for Admin and Public view

--- client-side/src/views/Admin/Films/FilmsDashboard.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div>
-		<h1>Films Dashboard</h1>
-		<h1>Films Dashboard</h1>
-		<h1>Films Dashboard</h1>
-	</div>
-</template>
---
+++ client-side/src/views/Admin/Movies/MoviesDashboard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Movies Dashboard</h1>
+    <h1>Movies Dashboard</h1>
+    <h1>Movies Dashboard</h1>
+  </div>
+</template>
